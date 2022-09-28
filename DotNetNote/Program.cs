81866affade33d3118a6d51385592d90b9a98964
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-09-28 17:17:02 +0900

    bootstrap , razor syntax, Route.Values , addTagHelper,(asp-page)

---
+++ DotNetNote/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); //정적인 HTML, CSS JS 를 브라우저 상에서 실행하게해주고싶을때   WWWROOT  (미들웨어) 기준은 PAGES 입
+
+//app.UseFileServer(); //index.html 등록시켜줌  기본문서 등록  wwwroot의 index.html을 기준으로 만들어줌 
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
