2a1714443501082428f0ae4071207584fc8bbb3f
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-09-29 16:01:47 +0900

    JsonFile  읽어와서  모델만드는 클래스 메서드 생성 후  읽어오기

--- DotNetNote/Models/Portfolio.cs
+++ DotNetNote/Models/Portfolio.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DotNetNote.Models
 {
@@ -10,6 +11,8 @@ namespace DotNetNote.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description{get;set ;}
+
+        [JsonPropertyName("img")] // json파일에서의 img이름 데이터를 갖어온다.  json파일로 변환할때는 img로 바꾼다. 
         public string Image { get; set; }
 
         public override string ToString()
---
+++ DotNetNote/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,23 @@
+﻿using DotNetNote.Models;
+using Microsoft.JSInterop.Implementation;
+using System.Text.Json;
+
+namespace DotNetNote.Services
+{
+    public class PortfolioServiceJsonFile 
+    {
+        public IEnumerable<Portfolio> GetPortfolios() // 파일경로의 json 파일에서 데이터를 읽어와 Model화 시켜서 반환해준다
+        {
+            var JsonFileName = @"C:\\Users\\hr\\Desktop\\code\\c#\\DotNetNote\\DotNetNote\\wwwroot\\Portfolios\\portfolio.json";
+
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(),options);
+                return portfolios;
+            }
+
+            
+        }
+    }
+}
