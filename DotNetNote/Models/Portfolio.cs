c0a12d3c37e674dd1d41ea4a11e81758d85b2d24
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-09-29 13:16:24 +0900

    인메모리 사용

---
+++ DotNetNote/Models/Portfolio.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace DotNetNote.Models
+{
+    public class Portfolio
+    {
+        /// <summary>
+        ///  모델 클래스
+        /// </summary>
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description{get;set ;}
+        public string Image { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Portfolio>(this); //json으로 변환시켜줌 
+        }
+
+    }
+}
